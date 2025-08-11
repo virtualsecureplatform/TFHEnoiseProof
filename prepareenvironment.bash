1f7c1edd1c9c96128fa79448865515b08f0c5943
nindanaoto <matsuoka.kotaro@gmail.com>
2025-08-11 05:56:44 +0000

    use bsdtar

--- prepareenvironment.bash
+++ prepareenvironment.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo add-apt-repository -y ppa:apptainer/ppa
 sudo apt update
-sudo apt install -y apptainer
+sudo apt install -y apptainer libarchive-tools
 sudo singularity build Isabella.sif docker://makarius/isabelle:Isabelle2025
 wget https://www.isa-afp.org/release/afp-current.tar.gz
-tar -xvf afp-current.tar.gz
+bsdtar -xvf afp-current.tar.gz
 rm afp-current.tar.gz
 mv afp-*/ afp-current
