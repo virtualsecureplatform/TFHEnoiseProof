f38b0dc50e50f890ac456837d180c99268e4cb8d
nindanaoto <matsuoka.kotaro@gmail.com>
2025-08-15 14:26:14 +0000

    fix bsdtar

--- prepareenvironment.bash
+++ prepareenvironment.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt update
+sudo apt install -y wget libarchive-tools
 wget https://isabelle.in.tum.de/dist/Isabelle2025_linux.tar.gz
 tar -xvf Isabelle2025.tar.gz
 wget https://www.isa-afp.org/release/afp-current.tar.gz
